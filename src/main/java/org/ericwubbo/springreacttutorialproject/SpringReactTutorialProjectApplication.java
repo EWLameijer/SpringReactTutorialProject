fa3f646d2aa96e9dc31cc9be48b3dad5f94d50b5
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2023-07-22 20:43:19 +0200

    Initial commit

---
+++ src/main/java/org/ericwubbo/springreacttutorialproject/SpringReactTutorialProjectApplication.java
@@ -0,0 +1,13 @@
+package org.ericwubbo.springreacttutorialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactTutorialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactTutorialProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ericwubbo/springreacttutorialproject/SpringReactTutorialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ericwubbo.springreacttutorialproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactTutorialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
